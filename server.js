3109cfbb48586f8ce7baafb5739b85f11af4b2d6
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-09-21 20:46:59 -1000

    reset tables

--- server.js
+++ server.js
@@ -13,16 +13,15 @@ const { Users, Topics, Messages } = require('./models');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-// app.use(bp.json());
-app.use(bp.urlencoded());
-app.use('/', require('./api/index.js'));
+app.use(bp.json());
+app.use('/api', require('./api/index.js'));
 
 app.get('*', (req, res) => {
   res.sendFile('./public/index.html', { root: __dirname });
 });
 
 app.listen(PORT, () => {
-  // db.sequelize.sync();
-  db.sequelize.sync({force:true});
+  db.sequelize.sync();
+  // db.sequelize.sync({force:true});
   console.log(`Server running on ${PORT}`);
 });
